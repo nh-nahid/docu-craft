dacd918efa1ddf0ec0c3c02b1c7cfec06ecdd6cd
Nahid Hossain <nahid4510@gmail.com>
2025-12-26 21:25:05 +0600

    md files added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from '../components/Header'
+import { getDocuments } from "@/lib/doc";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +25,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+      const allDocuments = getDocuments();
+    console.log(allDocuments);
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > <div className="h-full lg:ml-72 xl:ml-80">
+          <Header />
+          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+            <main className="flex-auto py-16">
+              <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+                <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+                  <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"></div>
+                </div>
+              </div>
+              {children}
+            </main>
+          </div>
+        </div>
       </body>
     </html>
   );
